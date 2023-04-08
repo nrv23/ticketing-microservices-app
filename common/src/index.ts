1b8ed84a1484754795cb09d6d10c15d3f4976d87
nrv23 <navemen23@hotmail.com>
2023-04-07 18:09:29 -0600

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -16,3 +16,5 @@ export * from './events/base-publisher';
 export * from './events/sujects';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
+
+export * from './events/types/order-status';
