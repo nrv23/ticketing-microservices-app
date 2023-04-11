0a3ceb3451d07f0a99f301aa5bd7ff68576e0af2
nrv23 <navemen23@hotmail.com>
2023-04-10 21:54:41 -0600

    Updates

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -6,6 +6,7 @@ export interface OrderCancelledEvent {
     subject: Subjects.OrderCancelled
     data: {
         id: string; // id de la orden a cancelar
+        version: number;
         ticket: {
             id: string;
         }
--- common/src/events/ticket-updated-event.ts
+++ common/src/events/ticket-updated-event.ts
@@ -4,6 +4,7 @@ export interface ITicketUpdatedEvent {
     subject: Subjects.TicketUpdated;
     data: {
         id: string;
+        version: number;
         title: string;
         price: number;
         userId:string;
